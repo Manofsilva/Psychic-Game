c425a58d758d1ec475e4baf4af1140d715e777db
Cesar Silva <silvacde@gmail.com>
2019-07-04 09:10:19 -0700

    added script tag and pseudocode

---
+++ javascript.js
@@ -0,0 +1,26 @@
+
+    {/* Creates an array that lists out all of the options (Rock, Paper, or Scissors). */}
+   
+    
+
+    {/* Creating variables to hold the number of wins, losses, and ties. They start at 0. */}
+   
+
+    {/* Create variables that hold references to the places in the HTML where we want to display things. */}
+  
+
+    {/* This function is run whenever the user presses a key. */}
+    
+
+      {/* Determines which key was pressed. */}
+      
+
+      {/* Randomly chooses a choice from the options array. This is the Computer's guess. */}
+      
+
+      {/* Reworked our code from last step to use "else if" instead of lots of if statements. */}
+
+      {/* This logic determines the outcome of the game (win/loss/tie), and increments the appropriate number */}
+      
+
+        {/* Display the user and computer guesses, and wins/losses/ties. */}
