d90e33105db6c11a4a315edf705e2d4dc8d1544c
Cesar Silva <silvacde@gmail.com>
2019-07-06 17:57:19 -0700

    array displays on document

--- javascript.js
+++ javascript.js
@@ -9,39 +9,41 @@ var possibleChoices = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l
     var guessesSoFar = [];
     var guessesLeft = 9;
 
-  // Create variables that hold references to the places in the HTML where we want to display things.
-  var winsText = document.getElementById("winsValue");
-  winsText.innerHTML = wins;
-
-  var lossesText = document.getElementById("lossesValue");
-  lossesText.innerHTML = losses;
   
-  var guessesLeftView = document.getElementById("guessesleftValue");
-  guessesLeftView.innerHTML = guessesLeft;
-
-  var guessesSoFarView = document.getElementById("guessessofarValue");
-  guessesSoFarView.innerHTML = guessesSoFar.join(", ");
-
-    
-    //  This function is run whenever the user presses a key. 
-        document.onkeyup = function(event) {
+    /* This function is run whenever the user presses a key. */
+    document.onkeyup = function(event) {
         var userGuess = String.fromCharCode(event.keyCode).toLowerCase();
         guessesSoFar.push(userGuess);
+       
+        // document.body.appendChild(winstext);
+        // document.write(winstext);
+        var possibleGuess = possibleChoices[Math.floor(Math.random() * possibleChoices.length)];
 
-    // created if statements.
-    if(userGuess === possibleGuess){
-        wins++;
-    }
-    else {
-        losses++;
-        guessesLeft--;
-    }
 
-    if(guessesLeft === 0){
-        reset();
-    }
+        if(userGuess === possibleGuess){
+            wins++;
+        }
+        else {
+            losses++;
+            guessesLeft--;
+        }
 
-    // reset variable
+        if(guessesLeft === 0){
+            reset();
+        }
+
+        var winsText = document.getElementById("winsValue");
+        winsText.innerHTML = wins;
+
+        var lossesText = document.getElementById("lossesValue");
+        lossesText.innerHTML = losses;
+
+        var guessesLeftView = document.getElementById("guessesleftValue");
+        guessesLeftView.innerHTML = guessesLeft;
+
+        var guessesSoFarView = document.getElementById("guessessofarValue");
+        guessesSoFarView.innerHTML = guessesSoFar.join(", ");
+    };
 
     var reset = function(){
         wins = 0;
